b2f50a97eb85444f2b3d171b4983c128c90255bc
rosarivera123 <rrivera@cristoreyny.org>
2017-01-26 13:28:08 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+ $("#clear").click(function() {
+  $("#results").empty();
+ });
+ $("#search").click(function() {
+var searchVal1 = ("#location").val();
+var searchVal2 = ("#food").val();
+
+ });
+});
